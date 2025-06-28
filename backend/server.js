d425a515fe4a195390d25aab354f577ffd52b791
Praveen Alva <user.praveenalva83@gmail.com>
2025-06-28 10:17:54 +0530

    Fixed MongoDB connection and removed syntax error

--- backend/server.js
+++ backend/server.js
@@ -5,20 +5,20 @@ const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 const appointmentRoutes = require('./routes/appointments');
 
-
-
-
-
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/appointments', appointmentRoutes);
 
-mongoose.connect(process.env.MONGO_URI,).
-then(() => console.log('MongoDB Connected'))
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
